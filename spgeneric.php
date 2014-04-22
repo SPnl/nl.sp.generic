15f17a98608f3165854af7dc5c008964c80a505c
Delano de Rooij <delano.derooij@civicoop.org>
2014-04-22 17:01:02 +0200

    Nieuwe memberships

--- spgeneric.php
+++ spgeneric.php
@@ -50,6 +50,66 @@ function spgeneric_civicrm_enable() {
   _spgeneric_location_type('Bezoekadres', 1);
   _spgeneric_static_group_value('Postvak', 1, 1);
   _spgeneric_static_group_value('Cursus', 14, 1);
+  _spgeneric_membership_type("Lid SP", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1);   
+  _spgeneric_membership_type("Lid SP en ROOD", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1);   
+  _spgeneric_membership_type("Lid ROOD", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1); 
+  _spgeneric_membership_type("Abonnee Tribune Betaald", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1);  
+  _spgeneric_membership_type("Abonnee Tribune Proef", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1);  
+  _spgeneric_membership_type("Abonnee Tribune Gratis", array(
+		'domain_id' => 1,
+		'member_of_contact_id' => 1,
+		'financial_type_id' => 2,
+		'duration_unit' => 'lifetime',
+		'duration_interval' => 1,
+		'is_active' => 1,
+		'version' => 3
+	)
+  , 1); 
   _spgeneric_membership_type("Abonnee SPanning", array(
 		'domain_id' => 1,
 		'member_of_contact_id' => 1,
@@ -83,6 +143,12 @@ function spgeneric_civicrm_disable() {
   _spgeneric_location_type('Bezoekadres', 0);
   _spgeneric_static_group_value('Postvak', 1, 0);
   _spgeneric_static_group_value('Cursus', 14, 0);
+  _spgeneric_membership_type("Lid SP", array(), 0);
+  _spgeneric_membership_type("Lid ROOD", array(), 0);
+  _spgeneric_membership_type("Lid SP en ROOD", array(), 0);
+  _spgeneric_membership_type("Abonnee Tribune Betaald", array(), 0);
+  _spgeneric_membership_type("Abonnee Tribune Proef", array(), 0);
+  _spgeneric_membership_type("Abonnee Tribune Gratis", array(), 0);
   _spgeneric_membership_type("Abonnee SPanning", array(), 0);
   _spgeneric_membership_type("Abonnee SPeciaal", array(), 0);
   return _spgeneric_civix_civicrm_disable();
